9346d2a76bd35d69123d4e8ae2f68a093baf5073
Giattino <40548@nelnet.net>
2021-06-02 15:18:00 -0400

    required before I can force

---
+++ index.js
@@ -0,0 +1,5 @@
+// Initializing context within the canvas
+var context = document.getElementById("canv00").getContext("2d");
+document.getElementById("canv00").addEventListener("click", function(){
+    this.style.backgroundColor = "red";
+});
